bc018a8841aebc500e74b3373ef49abf095a2088
K9Project <v.kh1992@mail.ru>
2024-07-19 11:41:18 +0400

    Modulus Added

--- PracticWorkVector.cpp
+++ PracticWorkVector.cpp
@@ -8,28 +8,40 @@ public:
     {}
     Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
     {}
-    int XX = x * x;
+    double lenght() const
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+    double GetModule()
+    {
+        return sqrt(pow(x, 2) + pow(y, 2) + pow(z, 2));
+    }
+
+
+    /*int XX = x * x;
     int YY = y * y;
     int ZZ = z * z;
     int Sum = XX + YY + ZZ;
-    int s = sqrt(Sum);
+    int s = sqrt(Sum);*/
 
     void Show()
     {
         std::cout << "\n " << x << " " << y << " " << z << "\n";
-        std::cout << "\n " << XX << " " << YY << " " << ZZ << "\n";
-        std::cout << " Sum XX+YY+ZZ = " << Sum << "\n " << s;
+        std::cout << " modulus (Lenght) = " << lenght() << "\n";
+        std::cout << " modulus (GetModule) = " << GetModule() << "\n";
+        /*std::cout << "\n " << XX << " " << YY << " " << ZZ << "\n";
+        std::cout << " Sum XX+YY+ZZ = " << Sum << "\n " << s;*/
     }
 
 private:
     double x;
     double y;
     double z;
-    double s;
+    /*double s;*/
 };
 
 int main()
 {
-    Vector v(3, 1, 7);
+    Vector v(1, 1, 1);
     v.Show();
 }
